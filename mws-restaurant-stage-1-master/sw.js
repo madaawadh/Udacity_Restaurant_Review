17056002741eb4d8e7fe44de3f9a32dcc2f21dd9
madaawadh <madaawadh1@gmail.com>
2019-05-13 21:18:46 +0300

    Udacity_project

---
+++ mws-restaurant-stage-1-master/sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', function(event){
+   
+  event.waitUntil(
+caches.open('witter-static-v1').then(function(cache) {
+  return cache.addAll(
+    [ '/',
+    '/index.html',
+    '/restaurant.html',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/css/styles.css',
+    '/data/restaurants.json',
+     '/img']
+     );
+    })
+  );
+});
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            if(response) return response;
+             return fetch(event.request); 
+        })
+
+    );
+});
